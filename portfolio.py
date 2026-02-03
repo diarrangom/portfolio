939bb222922bf5300384d88e21803cbf225d3f04
diarrangom <diarrangom1567@gmail.com>
2026-02-03 17:04:49 +0000

    Add files via upload

---
+++ portfolio.py
@@ -0,0 +1,57 @@
+import streamlit as st
+
+with st.sidebar:
+    st.write("**Mame Diarra NGOM**")
+    st.write("**Technicienne supérieure en Géomatique**")
+    st.write("📧 diarrangom1567@gmail.com")
+    st.write("📞 71 057 16 86")
+    st.write("📍 Guédiawaye, Dakar")
+
+st.header("💡 Mon profil")
+st.write("Je combine rigueur technique et interet pour l'analyse de données afin d'apporter des solutions cartographiques précises et opérationnelles")
+
+st.markdown("___")
+
+st.header("🏫 Projets académiques")
+st.write("""
+- Cartographie | Collecte de données spatiales dans au CEDT G15 et à Masalikul Jinan
+- Programmation Python | Développement d'application scientifique et de Web App 
+- Dessin de plan | Conception d'un plan architectural complet
+- Géographie | Réalisation de cartes thématiques sur ArcGIS, respectant les normes de sémiologie graphique et de mise en page""")
+
+st.markdown("___")
+
+st.header("✨ Compétences")
+st.write("""
+- Bonne maîtrise des logiciels SIG
+- Web-Mapping
+- Levé topographique
+- Cartographie numerique
+- Bonne maitrise de Canva
+- Compétences solides sur AutoCAD
+- Utilisation efficace des suites bureautiques et des outils de Microsoft 360
+- Pilotage de drone""")
+
+st.markdown("___")
+
+st.header("👩🏾‍🎓 Formations")
+st.write("2025-2026: Premiere année de BTS en Géomatique")
+st.write("2024-2025: Baccalaureat Scientifique")
+
+st.markdown("___")
+
+st.header("🏆 Expériences")
+st.write("""
+**Chargée de clientéle | Diaytar(E-commerce) | 2 mois | Mbour**
+- Relation client(Appels/Messages), gestion de commande et des relations fournisseurs, suivi des ventes
+
+**Vie associative | Dans mon ancienne école | GSEM**
+- Membre de la comité d'organisation liée aux activitées scolaires""")
+
+st.markdown("___")
+
+st.header("🚀 Projets personnels")
+
+st.markdown("___")
+
+st.header("📰 Articles et publications")
